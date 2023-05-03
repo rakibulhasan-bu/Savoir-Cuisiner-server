bfe7364bf6961c2b07c283839bd22513a3c2644b
Rakibul Hasan <rakibulhasanbu@gmail.com>
2023-05-04 01:54:47 +0600

    create data in json formate

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const port = process.env.PORT || 5000;
 
 const chefsData = require('./data/chefsData.json')
 
-// app.use(cors());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Chef server is running')
 });
 
-// app.get('/chefs', (req, res) => {
-//     res.send(chefsData);
-// })
+app.get('/chefs', (req, res) => {
+    res.send(chefsData);
+})
 
 // app.get('/chefs/:id', (req, res) => {
 //     const id = parseInt(req.params.id);
